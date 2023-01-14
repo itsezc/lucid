238c20bcf9a8b92bd20afeec6c19f1db63d553cf
itsezc <chirub@foretag.co>
2023-01-14 22:53:19 +0000

    add(orm): string & time types

---
+++ libs/orm/src/types/string.ts
@@ -0,0 +1,62 @@
+export class SurealString {
+	public static concat(str: string) {
+		return `string::concat(${str})`;
+	}
+
+	public static endsWith(str: string) {
+		return `string::endsWith(${str})`;
+	}
+
+	public static join(str: string) {
+		return `string::join(${str})`;
+	}
+
+	// @ts-ignore
+	public static length(str: string) {
+		return `string::length(${str})`;
+	}
+
+	public static lowercase(str: string) {
+		return `string::lowercase(${str})`;
+	}
+
+	public static repeat(str: string) {
+		return `string::repeat(${str})`;
+	}
+
+	public static replace(str: string) {
+		return `string::replace(${str})`;
+	}
+
+	public static reverse(str: string) {
+		return `string::reverse(${str})`;
+	}
+
+	public static slice(str: string) {
+		return `string::slice(${str})`;
+	}
+
+	public static slug(str: string) {
+		return `string::slug(${str})`;
+	}
+
+	public static split(str: string) {
+		return `string::split(${str})`;
+	}
+
+	public static startsWith(str: string) {
+		return `string::startsWith(${str})`;
+	}
+
+	public static trim(str: string) {
+		return `string::trim(${str})`;
+	}
+
+	public static uppercase(str: string) {
+		return `string::uppercase(${str})`;
+	}
+
+	public static words(str: string) {
+		return `string::words(${str})`;
+	}
+}
