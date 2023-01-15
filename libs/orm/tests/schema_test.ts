a817cff11fef79bf318f97caaa32bcf4dfe52f7b
Drew Ridley <drew.ridley03@gmail.com>
2023-01-15 07:15:55 -0500

    Base parse iteration; no anon types, arrays or perms.

--- libs/orm/tests/schema_test.ts
+++ libs/orm/tests/schema_test.ts
@@ -20,9 +20,13 @@ sourceFile.getChildren().forEach(child => {
     classes.forEach(classNode => {
         //Each class delcaration,
 
-        //The table identifier if it exists,
+        //The node identifying the table (ie the Table Name)
         const identifier = classNode.getChildren().find(n => ts.isIdentifier(n));
+        
+        //The class decorator
         let decorator: ts.Node | null;
+
+        let fields: ts.Node[];
     
 
         //Finds and verifies that the decorator matches.
@@ -38,21 +42,73 @@ sourceFile.getChildren().forEach(child => {
                 if (ident.getText() == 'Table') {
                     decorator = dec;
                 }
-            })
+            });
+
+            fields = ch.getChildren().filter(n => ts.isPropertyDeclaration(n));
         });
 
-        console.log(identifier.getText());
+        //TODO: convert table name to snake_case.
+        //TODO: parse permissions here:
 
-        console.log(camel_)
+        let tableDef = `DEFINE TABLE ${identifier.getText()} schemaless\n\tPERMISSIONS\n\t\tNONE`;
+        
+
+
+        fields.forEach(f => {
+            tableDef += parseField(identifier.getText(), f.getChildren().find(n => n.kind == ts.SyntaxKind.Identifier).getText(), f);
+        })
 
 
     })
 })
 
+//Parses a node and returns a SurrealQL schema string for it.
+function parseField(table: string, name: string, f: ts.Node) {
+    //TODO: parse decorator for permissions, defaults and constraints.
+    //The decorator will be the only possible SyntaxList on the field.
+    const decorator = f.getChildren().find(n => n.kind == ts.SyntaxKind.SyntaxList);
 
-//Generates a full SurrealQL table definition for the node. Expects node to be a ClassDefinition.
-function generateTable(node: ts.Node): string {
+    //Whether this field is optional or not (ie contains ?)
+    const optional: boolean = f.getChildren().some(n => n.kind == ts.SyntaxKind.QuestionToken);
 
+    let type;
+
+    //Assign type string if it doesn't already have a type.
+    type ||= f.getChildren()?.find(n => n.kind == 152)?.getText();
+
+    //Assign tpe number if it doesn't already have a type.
+    type ||= f.getChildren()?.find(n => n.kind == 148)?.getText();
+    
+    
+    //The Type Reference (if it exists). Since we already assume this type exists,
+    //We just need to clarify to Surreal that its a table reference.
+    const ref = f.getChildren().find(n => n.kind == ts.SyntaxKind.TypeReference);
+
+    if (ref) {
+        //Type needs to be record(underlyingType).
+        const ident = ref.getChildren().find(c => ts.isIdentifier(c));
+
+        //This does not work for Date types.
+        type = `record(${ident.getText()})`;
+    }
+
+    //The array type, if it is an array. This type will contain the inner type/ident.
+    const arr = f.getChildren().find(n => n.kind == ts.SyntaxKind.ArrayType);
+
+    if (arr) {
+        type = 'array';
+    }
+
+    let fieldStr = `DEFINE FIELD ${name} ON TABLE ${table} SCHEMAFULL\n\tTYPE ${type}`;
+
+    if (arr) {
+        //TODO: verify that getChildAt does not lead to bugs.
+        fieldStr += parseField(table, name + '.*', f.getChildAt(0));
+    }
+
+    fieldStr += '\n\t\tPERMISSIONS';
+
+    console.log(fieldStr);
 }
 
 //Generates a full SurrealQL field definition for the node. Expects node to be a PropertyDeclaration.
