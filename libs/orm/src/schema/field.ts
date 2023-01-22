c7e471f5d4c9dcf074ae7bcc4714156dd0c2935c
Drew Ridley <drew.ridley03@gmail.com>
2023-01-21 20:08:57 -0500

    moved array parsing.

--- libs/orm/src/schema/field.ts
+++ libs/orm/src/schema/field.ts
@@ -1,7 +1,8 @@
 import * as ts from 'byots';
 
 
-export function parseField(n: ts.Node, tableName: string, depth = 1): string {
+//TODO: refactor so that this function can parse any field node, even a TypeLiteral, ArrayType, or Identifier
+export function parseField(n: ts.Node, tableName: string, baseName = '', typeLit = false): string {
     const sl = n.getChildren().find(n => n.kind == ts.SyntaxKind.SyntaxList);
     const dec = sl?.getChildren().find(n => ts.isDecorator(n));
 
@@ -12,7 +13,10 @@ export function parseField(n: ts.Node, tableName: string, depth = 1): string {
     const optional = n.getChildren().find(n => n.kind == ts.SyntaxKind.QuestionToken);
     const required = n.getChildren().find(n => n.kind == ts.SyntaxKind.ExclamationToken);
 
-    if (!optional && !required && depth == 1) {
+    //Whether we have a type literal or not.
+
+    //! and ? can only be used in a Class, not an anonymous object.
+    if (!optional && !required && !typeLit) {
         console.error(`Cannot parse field ${n.getText()}. Field must be either optional or required, annotated with ! or ? respectively.`);
         process.exit();
     }
@@ -36,12 +40,25 @@ export function parseField(n: ts.Node, tableName: string, depth = 1): string {
         'GeoPolygon': 'polygon',
         'GeoMultiPolygon': 'multipolygon',
         'GeoCollection': 'collection',
+        'typeLiteral': 'object'
     };
 
     
     let subFields = [];
 
     n.getChildren().forEach(child => {
+        if (child.kind == ts.SyntaxKind.TypeLiteral) {
+            const sl = child.getChildren().find(n => n.kind == ts.SyntaxKind.SyntaxList);
+            const propSigs = sl?.getChildren().filter(n => ts.isPropertySignature(n));
+
+            propSigs?.forEach(prop => {
+                //For each inline property defined here, create a nested type and append to field.
+                //subFields.push(parseField(prop, tableName, ''));
+                subFields.push(parseField(prop, tableName, ident.getText() + '.', true));
+            });
+
+            fieldType = 'object';
+        }
         if(primitiveTypes[child.getText()]) {
             fieldType = primitiveTypes[child.getText()];
         }
@@ -54,23 +71,15 @@ export function parseField(n: ts.Node, tableName: string, depth = 1): string {
             else if (child.kind == ts.SyntaxKind.Identifier) {
                 //This is a user type that we must resolve, or a table reference (must be resolved).
             }
-            else if (child.kind == ts.SyntaxKind.TypeLiteral) {
-                const sl = child.getChildren().find(n => n.kind == ts.SyntaxKind.SyntaxList);
-                const propSigs = sl?.getChildren().filter(n => ts.isPropertySignature(n));
-
-                propSigs?.forEach(prop => {
-                    //For each inline property defined here, create a nested type and append to field.
-                    //subFields.push(parseField(prop, tableName, ''));
-                })
-            }
         }
     });
 
     // bool (SQL) boolean
 
-    let fieldSchema = `DEFINE FIELD ${ident.getText()} ON TABLE ${tableName} TYPE ${fieldType}`;
+    let fieldSchema = `DEFINE FIELD ${baseName + ident.getText()} ON TABLE ${tableName} TYPE ${fieldType}`;
 
-    fieldSchema += subFields.join('\n');
+    //TODO: discover why these nested objects are not inserted correctly.
+    fieldSchema += subFields.length > 0 ? '\n': '' + subFields.join('\n');
 
     return fieldSchema;
 }
