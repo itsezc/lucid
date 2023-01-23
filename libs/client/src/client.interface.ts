7546fb14cbfb462435d8874e881881e40589495b
Drew Ridley <drew.ridley03@gmail.com>
2023-01-23 00:29:05 -0500

    started work on interface.

--- libs/client/src/client.interface.ts
+++ libs/client/src/client.interface.ts
@@ -1,6 +1,15 @@
+import { ISurrealScope } from "@surreal-tools/orm";
+
 export default interface ISurrealConnector {
     query<T>(query: string): Promise<Array<T>>;
 
-    signin(username: string, password: string): Promise<boolean>;
-    signup(username: string, password: string): Promise<boolean>;
+    signin<S>(args: TSurrealAuthArgs | S): Promise<boolean>;
+    signup<S>(args: TSurrealAuthArgs | S): Promise<boolean>;
+}
+
+type TSurrealAuthArgs = {
+    user?: string;
+    pass?: string;
+    DB?: string;
+    NS?: string;
 }
--- libs/client/src/client.rest.ts
+++ libs/client/src/client.rest.ts
@@ -1,17 +1,74 @@
+
+import { ISurrealScope, Model, Table } from '@surreal-tools/orm';
 import ISurrealConnector from './client.interface';
 
-export default class SurrealRest implements ISurrealConnector {
+type TSurrealResponse<T> = {
+    result: T[],
+    error: {}[],
+};
+
+type ExtractVars<T extends ISurrealScope> = T extends ISurrealScope<infer Vars> ? Vars : never;
+
+class Account extends Model {}
+
+type ScopeType = { $username: string; $passKey: string; };
+
+const AccountScope: ISurrealScope<ScopeType> = {
+    name: 'account',
+    timeout: '1h',
+    table: Account,
+    signin: () => {},
+    siginup: () => {},
+}
+
+
+export default class SurrealRest
+    implements ISurrealConnector 
+{
+    constructor(
+        public host: string,
+        private token: string,
+        private NS: string,
+        private DB: string,
+        private SC: string,
+    ) {}
+
     //Using a token must be done within the query method since this connector is stateless.
     async query<T>(query: string): Promise<T[]> {
+        const res = await fetch(`${this.host}/sql`, {
+            headers: {
+                NS: this.NS,
+                DB: this.DB,
+                Accept: 'application/json',
+                Authorization: `Bearer ${this.token}`
+            },
+            body: query
+        }) as unknown as TSurrealResponse<T>;
 
-        return [];
+        return res.result;
     }
 
-    async signin(username: string, password: string): Promise<boolean> {
-        return false;
+
+    async signin<S extends ISurrealScope>(args: ExtractVars<S>): Promise<string> {
+        const res = await fetch(`${this.host}/signin`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                NS: this.NS,
+                DB: this.DB,
+            },
+            body: JSON.stringify({
+                NS: this.NS,
+                DB: this.DB,
+                SC: this.SC,
+                username: 'abc',
+                password: 'def',
+                ...args
+            })
+        });
+
+        return res;
     }
 
-    async signup(username: string, password: string): Promise<boolean> {
-        return false;
-    }
 }
+
